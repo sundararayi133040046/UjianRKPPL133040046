284fadacceaa7042b35f0e59c91047afeb815bdc
sundararayi133040046 <sundararayi@mail.unpas.ac.id>
2016-05-13 14:41:09 +0700

    second commit

---
+++ src/main/java/rkppl/main/logging/MainTest.java
@@ -0,0 +1,12 @@
+package rkppl.main.logging;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello Jawa");
+		System.out.println("Hello Jawa Barat");
+	}
+
+}
